a0dfe271b438d20e07fa3ca04d8d1ef910d05cc3
wildarch <daandegraaf9@gmail.com>
2017-07-18 17:42:38 +0200

    Cleanup, make test fetch_endpoints internal

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 #![recursion_limit = "1024"]
-extern crate xml;
-extern crate reqwest;
 
 #[macro_use]
 extern crate error_chain;
@@ -13,21 +11,23 @@ error_chain! {
     foreign_links {
         Io(::std::io::Error);
         Reqwest(::reqwest::Error);
+        Url(::reqwest::UrlError);
     }
 }
 
-use errors::*;
-
-use std::io::Read;
+extern crate xml;
 use xml::reader::{EventReader, XmlEvent};
+extern crate reqwest;
+use reqwest::Url;
+
 
 const SPEC_BASE_URL : &'static str = "https://start.exactonline.nl/docs/";
 const SPEC_OVERVIEW : &'static str = "HlpRestAPIResources.aspx";
 const SPEC_DETAIL : &'static str = "HlpRestAPIResourcesDetails.aspx";
 
-pub fn fetch_endpoints() -> Result<Vec<String>> {
-    let url = SPEC_BASE_URL.to_owned() + SPEC_OVERVIEW;
-    let response = reqwest::get(&url)?;
+pub fn fetch_endpoints() -> Result<Vec<Url>> {
+    let overview_url = SPEC_BASE_URL.to_owned() + SPEC_OVERVIEW;
+    let response = reqwest::get(&overview_url)?;
     let parser = EventReader::new(response);
     let mut results = Vec::new();
     for e in parser {
@@ -37,7 +37,8 @@ pub fn fetch_endpoints() -> Result<Vec<String>> {
                     for attr in attributes {
                         if attr.name.local_name == "href" && attr.value.starts_with(SPEC_DETAIL) {
                             println!("{}", &attr);
-                            results.push(SPEC_BASE_URL.to_owned() + &attr.value);
+                            let url = Url::parse(&(SPEC_BASE_URL.to_owned() + &attr.value))?;
+                            results.push(url);
                         }
                     }
                 }
@@ -50,4 +51,18 @@ pub fn fetch_endpoints() -> Result<Vec<String>> {
         }
     }
     Ok(results)
+}
+
+#[test]
+fn it_fetches_endpoint_url() {
+    const SPEC_URL_PREFIX: &'static str = "https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=";
+
+    let urls = fetch_endpoints().expect("Successfully fetched endpoints");
+    assert!(!urls.is_empty());
+    for u in urls {
+        if !u.as_str().starts_with(SPEC_URL_PREFIX) {
+            println!("Invalid endpoint: {}", u);
+            panic!();
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate exact_openapi_gen;
 
 fn main() {
+    /*
     let endpoints = exact_openapi_gen::fetch_endpoints().unwrap();
     for e in endpoints {
         println!("{}", e);
-    }
+    }*/
 }
