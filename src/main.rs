8146ed251e13cd19b6639e76a9fb6570c0556c4a
wildarch <daandegraaf9@gmail.com>
2017-07-22 11:56:25 +0200

    Change error message from 'yaml' to 'json'

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
         }).collect();
 
     let spec = exact_openapi_gen::build_spec(endpoints);
-    let json = openapi::to_json(&spec.expect("Valid spec")).expect("Valid yaml spec");
+    let json = openapi::to_json(&spec.expect("Valid spec")).expect("Valid json spec");
     let mut file = File::create("api.json").expect("File opened");
     file.write_all(json.as_bytes()).expect("Successfully written to file");
 
